f1828e64e1c68086d77fa3555d2377eb9daa5f4e
jackgit1214 <hongyullj@sohu.com>
2017-01-15 14:39:39 +0800

    日志服务

--- framework-core/src/main/java/com/system/mybatis/service/impl/SystemLogServiceImpl.java
+++ framework-core/src/main/java/com/system/mybatis/service/impl/SystemLogServiceImpl.java
@@ -1,6 +1,7 @@
 package com.system.mybatis.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.framework.common.util.UUIDUtil;
 import com.framework.mybatis.dao.Base.BaseDao;
@@ -9,6 +10,7 @@ import com.system.model.SysLog;
 import com.system.mybatis.dao.SysLogMapper;
 import com.system.mybatis.service.ISystemLogService;
 
+@Component
 public class SystemLogServiceImpl extends AbstractBusinessService<SysLog>
 		implements ISystemLogService {
 

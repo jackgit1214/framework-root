7e1d1def0a9c5716a6af684f2323c76c5f3f4841
jackgit1214 <hongyullj@sohu.com>
2017-02-12 11:37:04 +0800

    添加datamappercrud

--- framework-core/src/main/java/com/framework/mybatis/service/AbstractBusinessService.java
+++ framework-core/src/main/java/com/framework/mybatis/service/AbstractBusinessService.java
@@ -15,6 +15,7 @@ import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import com.framework.mybatis.dao.Base.BaseDao;
 import com.framework.mybatis.dao.Base.IDataMapper;
 import com.framework.mybatis.dao.Base.IDataMapperByPage;
+import com.framework.mybatis.dao.Base.IDataMapperCRUD;
 import com.framework.mybatis.dao.Base.IDataMapperWithBlob;
 import com.framework.mybatis.model.QueryModel;
 import com.framework.mybatis.util.PageResult;
@@ -26,28 +27,51 @@ public abstract class AbstractBusinessService<T> implements IBusinessService<T> 
 	private IDataMapper<T> dataMapper;
 	private IDataMapperWithBlob<T> dataMapperWithBlob;
 	private IDataMapperByPage<T> dataMapperByPage;
+	private IDataMapperCRUD<T> dataMapperCRUD;
 
 	protected final Log log = LogFactory.getLog(this.getClass());
-	
+
 	SimpleKeyGenerator k = new SimpleKeyGenerator();
 
 	@Cacheable(value = "CustomerCache", key = "#root.target")
 	public List<T> findAllObjects() {
 		this.initDataMapper();
-		return this.dataMapper.selectByCondition(null);
+
+		if (this.dataMapper != null)
+			return this.dataMapper.selectByCondition(null);
+		else if (this.dataMapperCRUD != null)
+			return this.dataMapperCRUD.selectByCondition(null);
+		else {
+			this.log.debug("未实现dataMapper或dataMapperCRUD接口");
+			return null;
+		}
 	}
 
 	@Override
 	public T findObjectById(Object object) {
 		this.initDataMapper();
-		return this.dataMapper.selectByPrimaryKey(object);
+		if (this.dataMapper != null)
+			return this.dataMapper.selectByPrimaryKey(object);
+		else if (this.dataMapperCRUD != null)
+			return this.dataMapperCRUD.selectByPrimaryKey(object);
+		else {
+			this.log.debug("未实现dataMapper或dataMapperCRUD接口");
+			return null;
+		}
 	}
 
 	@Override
 	@Cacheable(value = "CustomerCache", key = "#queryModel.condition")
 	public List<T> findObjects(QueryModel queryModel) {
 		this.initDataMapper();
-		return this.dataMapper.selectByCondition(queryModel);
+		if (this.dataMapper != null)
+			return this.dataMapper.selectByCondition(queryModel);
+		else if (this.dataMapperCRUD != null)
+			return this.dataMapperCRUD.selectByCondition(queryModel);
+		else {
+			this.log.debug("未实现dataMapper或dataMapperCRUD接口");
+			return null;
+		}
 	}
 
 	@Override
@@ -63,18 +87,18 @@ public abstract class AbstractBusinessService<T> implements IBusinessService<T> 
 		return page;
 	}
 
-	
-	public PageResult<T> findObjectsByPage(IDataMapper baseDao,String methodName,QueryModel queryModel,
-			PageResult<T> page) throws Exception {
-		Proxy.newProxyInstance(this.dataMapper.getClass().getClassLoader(), new Class[]{},
-					new MapperProxyHandler(baseDao));
-		
+	public PageResult<T> findObjectsByPage(IDataMapper baseDao,
+			String methodName, QueryModel queryModel, PageResult<T> page)
+			throws Exception {
+		Proxy.newProxyInstance(this.dataMapper.getClass().getClassLoader(),
+				new Class[] {}, new MapperProxyHandler(baseDao));
+
 		List<T> pageDatas = this.dataMapperByPage.selectByCondition(queryModel,
 				page);
 		page.setPageDatas(pageDatas);
 		return page;
 	}
-	
+
 	@PostConstruct
 	public abstract BaseDao getDao();
 
@@ -82,18 +106,24 @@ public abstract class AbstractBusinessService<T> implements IBusinessService<T> 
 	@PostConstruct
 	private void initDataMapper() {
 		BaseDao baseDao = this.getDao();
-		if (baseDao instanceof IDataMapper){
+		if (baseDao instanceof IDataMapper) {
 			this.dataMapper = (IDataMapper<T>) baseDao;
 		}
 		if (baseDao instanceof IDataMapperWithBlob)
 			this.dataMapperWithBlob = (IDataMapperWithBlob<T>) baseDao;
-		if (baseDao instanceof IDataMapperByPage){
+
+		if (baseDao instanceof IDataMapperByPage) {
 			this.dataMapperByPage = (IDataMapperByPage<T>) baseDao;
+		}
+
+		if (baseDao instanceof IDataMapperCRUD) {
+			this.dataMapperCRUD = (IDataMapperCRUD<T>) baseDao;
 		}
 	}
 
 	class MapperProxyHandler implements InvocationHandler {
 		private Object proxied;
+
 		public MapperProxyHandler(Object proxied) {
 			this.proxied = proxied;
 		}
@@ -104,6 +134,4 @@ public abstract class AbstractBusinessService<T> implements IBusinessService<T> 
 		}
 	}
 
-
-	
 }

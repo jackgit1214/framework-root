dc4b0c27b7b9db251fd5af719efbb93d0e66a6f5
jackgit1214 <hongyullj@sohu.com>
2017-03-25 17:52:08 +0800

    修正重复提交问题，只有方法执行成功时才能去除token

--- framework-core/src/main/java/com/framework/web/interceptor/DuplicateSubmissionInterceptor.java
+++ framework-core/src/main/java/com/framework/web/interceptor/DuplicateSubmissionInterceptor.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.framework.common.anaotation.DuplicateSubmission;
@@ -17,55 +18,84 @@ import com.framework.web.util.SessionManager;
 
 /**
  * 
- * @author lilj
- * 防止重复提交过滤器
+ * @author lilj 防止重复提交过滤器
  */
 public class DuplicateSubmissionInterceptor extends HandlerInterceptorAdapter {
 
 	private final Log log = LogFactory.getLog(this.getClass());
+
+	private boolean isSuccess = false;
+
 	@Override
-    public boolean preHandle(HttpServletRequest request,
-            HttpServletResponse response, Object handler) throws Exception {
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+
+		this.isSuccess = false;
 		HttpSession session = request.getSession();
-		SessionManager sessionManager = (SessionManager)session.getAttribute("sessionManager");
-        if (sessionManager != null) {
-            HandlerMethod handlerMethod = (HandlerMethod) handler;
-            Method method = handlerMethod.getMethod();
- 
-            DuplicateSubmission annotation = method.getAnnotation(DuplicateSubmission.class);
-            if (annotation != null) {
-                boolean needSaveSession = annotation.needSaveToken();
-                if (needSaveSession) {
-                	RandomGUID token = new RandomGUID();
-                    request.getSession(false).setAttribute("token", token.toString());
-                }
- 
-                boolean needRemoveSession = annotation.needRemoveToken();
-                if (needRemoveSession) {
-                    if (isRepeatSubmit(request)) {
-                        log.warn("请不要重复提交,[用户:" + sessionManager.getUser().toString() + ",url:"
-                                + request.getServletPath() + "]");
-                        return false;
-                    }
-                    request.getSession(false).removeAttribute("token");
-                }
-            }
-        }
-        return true;
-    }
- 
-    private boolean isRepeatSubmit(HttpServletRequest request) {
-        String serverToken = (String) request.getSession(false).getAttribute("token");
-        if (serverToken == null) {
-            return true;
-        }
-        String clinetToken = request.getParameter("token");
-        if (clinetToken == null) {
-            return true;
-        }
-        if (!serverToken.equals(clinetToken)) {
-            return true;
-        }
-        return false;
-    }
+		SessionManager sessionManager = (SessionManager) session
+				.getAttribute("sessionManager");
+		if (sessionManager != null) {
+			HandlerMethod handlerMethod = (HandlerMethod) handler;
+			Method method = handlerMethod.getMethod();
+
+			DuplicateSubmission annotation = method
+					.getAnnotation(DuplicateSubmission.class);
+			if (annotation != null) {
+				boolean needSaveSession = annotation.needSaveToken();
+				if (needSaveSession) {
+					RandomGUID token = new RandomGUID();
+					request.getSession(false).setAttribute("token",
+							token.toString());
+					this.isSuccess = false;
+				}
+
+				boolean needRemoveSession = annotation.needRemoveToken();
+				if (needRemoveSession) {
+					if (isRepeatSubmit(request) && this.isSuccess) {
+						log.warn("请不要重复提交,[用户:"
+								+ sessionManager.getUser().toString() + ",url:"
+								+ request.getServletPath() + "]");
+						return false;
+					}
+					request.getSession(false).removeAttribute("token");
+				}
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		log.warn("----------------afterCompletion----------------");
+		super.afterCompletion(request, response, handler, ex);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		log.warn("---------------postHandle-----------------");
+		this.isSuccess = true;
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	private boolean isRepeatSubmit(HttpServletRequest request) {
+		String serverToken = (String) request.getSession(false).getAttribute(
+				"token");
+		if (serverToken == null) {
+			return true;
+		}
+		String clinetToken = request.getParameter("token");
+		if (clinetToken == null) {
+			return true;
+		}
+		if (!serverToken.equals(clinetToken)) {
+			return true;
+		}
+		return false;
+	}
 }

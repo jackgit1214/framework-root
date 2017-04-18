3f0119c3d4d96745cdf12dac84fe13cda63d68cd
jackgit1214 <hongyullj@sohu.com>
2017-04-18 17:13:53 +0800

    影像常量类

---
+++ framework-resources/src/main/java/com/resources/ResConstant.java
@@ -0,0 +1,14 @@
+package com.resources;
+
+public interface ResConstant {
+
+	/**
+	 * 资源类型 附件类型: 0 视频、1图片、2音频、3文档（pdf与word）等
+	 * */
+
+	public static final int RES_VIDEO = 0;
+	public static final int RES_IMAGE = 1;
+	public static final int RES_AUDIO = 2;
+	public static final int RES_DOC = 3;
+
+}

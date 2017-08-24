980572904682e40fab25793cf8ebcc581e3f4209
jackgit1214 <hongyullj@sohu.com>
2017-08-24 14:13:17 +0800

    修改组件重复初始化的问题，例如make-switch

--- museum/src/main/webapp/resources/js/system.js
+++ museum/src/main/webapp/resources/js/system.js
@@ -453,15 +453,14 @@ $.SystemApp.initComponents = function() {
 	if ($('input:checkbox, input:radio')[0]) {
 
 		// Checkbox + Radio skin
-		$(
-				'input:checkbox:not([data-toggle="buttons"] input, .make-switch input), input:radio:not([data-toggle="buttons"] input)')
+		$('input:checkbox:not([data-toggle="buttons"] input, .make-switch input), input:radio:not([data-toggle="buttons"] input)')
 				.iCheck({
 					checkboxClass : 'icheckbox_minimal',
 					radioClass : 'iradio_minimal',
 					increaseArea : '20%' // optional
 				});
 
-		$('.make-switch').bootstrapSwitch();
+		$('.make-switch:not(.has-switch)').bootstrapSwitch();
 
 		// Checkbox listing
 		var parentCheck = $('.list-parent-check');

c0a116f391941c3ad0b49b6552d053d019feeb63
jackgit1214 <hongyullj@sohu.com>
2018-03-06 11:24:24 +0800

    bug处理

--- framework-core/src/main/java/com/framework/common/util/TreeDataUtil.java
+++ framework-core/src/main/java/com/framework/common/util/TreeDataUtil.java
@@ -7,34 +7,37 @@ import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.framework.common.model.ITreeData;
+import com.framework.model.TreeData;
 
 /**
  * @author lilj
  *
  */
-public class TreeDataUtil{
+public class TreeDataUtil {
 
-	public static JsonNode ConvertToJsonData(ITreeData treeData) throws Exception{
-		
+	public static JsonNode ConvertToJsonData(TreeData treeData)
+			throws Exception {
+
 		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode  jsonnode = objectMapper.readTree(objectMapper.writeValueAsString(treeData));
+		JsonNode jsonnode = objectMapper.readTree(objectMapper
+				.writeValueAsString(treeData));
 		return jsonnode;
 	}
-	
-	public static JsonNode ConvertToJsonData(List<?> treeDatas) throws Exception{
+
+	public static JsonNode ConvertToJsonData(List<?> treeDatas)
+			throws Exception {
 		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode  jsonnode = objectMapper.readTree(objectMapper.writeValueAsString(treeDatas));
+		JsonNode jsonnode = objectMapper.readTree(objectMapper
+				.writeValueAsString(treeDatas));
 		return jsonnode;
 	}
-	
-	public static String ConvertToString(ITreeData treeData) throws Exception{
+
+	public static String ConvertToString(TreeData treeData) throws Exception {
 		return ConvertToJsonData(treeData).toString();
 	}
 
-	public static String ConvertToString(List<?> treeDatas) throws Exception{
+	public static String ConvertToString(List<?> treeDatas) throws Exception {
 		return ConvertToJsonData(treeDatas).toString();
-	}	
+	}
 
-	
 }

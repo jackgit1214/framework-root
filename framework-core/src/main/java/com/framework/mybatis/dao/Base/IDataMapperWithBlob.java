6796cd5693751d318cc58a43323101728b5b131d
lilj <hongyullj@sohu.com>
2020-01-29 17:49:28 +0800

    增加分页的参数

--- framework-core/src/main/java/com/framework/mybatis/dao/Base/IDataMapperWithBlob.java
+++ framework-core/src/main/java/com/framework/mybatis/dao/Base/IDataMapperWithBlob.java
@@ -2,17 +2,18 @@ package com.framework.mybatis.dao.Base;
 
 import java.util.List;
 
+import com.framework.mybatis.util.PageResult;
 import org.apache.ibatis.annotations.Param;
 
 import com.framework.mybatis.model.QueryModel;
 
 public interface IDataMapperWithBlob<T> extends BaseDao {
 
-    List<T> selectByConditionWithBLOBs(@Param("queryModel") QueryModel queryModel);
-    
+    List<T> selectByConditionWithBLOBs(@Param("queryModel") QueryModel queryModel,@Param("page") PageResult<T> page);
+
     int updateByConditionWithBLOBs(@Param("record") T record, @Param("queryModel") QueryModel queryModel);
-    
+
     int updateByPrimaryKeyWithBLOBs(T record);
-    
+
     T selectBlobByPrimaryKey(String id);
 }
